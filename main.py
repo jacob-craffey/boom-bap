a7ce29c80351efa56a0ef907c767b6fe7ac8cd8f
Desmin <deslittle2015@gmail.com>
2018-03-29 11:52:20 -0400

    Added gitignore and update main.py to comply with 3.6 requirements.

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from Tkinter import *
-import tkFileDialog as filedialog
+from tkinter import *
+import tkinter.filedialog as filedialog
 import subprocess
 import sys
 
+
 def open_folder(label):
-    f = filedialog.askopenfilename(filetypes = (("WAV files", "*.wav"),("All files","*.*")))
-    f = f.split("boom-bap",1)[1]
+    f = filedialog.askopenfilename(filetypes=(("WAV files", "*.wav"), ("All files", "*.*")))
+    f = f.split("boom-bap", 1)[1]
     label["text"] = f
 
+
 def try_instruments(kick, snare, hihat):
-    print kick
-    print snare
-    print hihat
-    
+    print(kick)
+    print(snare)
+    print(hihat)
+
 
 def initialize_menu():
     root = Tk()
@@ -27,10 +29,11 @@ def initialize_menu():
     lbl_snare = Label(root, text=txt_snare)
     lbl_hihat = Label(root, text=txt_hihat)
 
-    btn_kick = Button(root, text="Add Kick", command=lambda:open_folder(lbl_kick))
-    btn_snare = Button(root, text="Add Snare", command=lambda:open_folder(lbl_snare))
-    btn_hihat = Button(root, text="Add HiHat", command=lambda:open_folder(lbl_hihat))
-    btn_tryout = Button(root, text="Try out the Instruments", command=lambda:try_instruments(lbl_kick["text"],lbl_snare["text"],lbl_hihat["text"]))
+    btn_kick = Button(root, text="Add Kick", command=lambda: open_folder(lbl_kick))
+    btn_snare = Button(root, text="Add Snare", command=lambda: open_folder(lbl_snare))
+    btn_hihat = Button(root, text="Add HiHat", command=lambda: open_folder(lbl_hihat))
+    btn_tryout = Button(root, text="Try out the Instruments",
+                        command=lambda: try_instruments(lbl_kick["text"], lbl_snare["text"], lbl_hihat["text"]))
 
     btn_kick.grid(row=0, column=0, padx=10, pady=10)
     btn_snare.grid(row=1, column=0, padx=10, pady=10)
@@ -43,8 +46,10 @@ def initialize_menu():
 
     mainloop()
 
+
 def main():
     initialize_menu()
+
 
 if __name__ == "__main__":
     main()
