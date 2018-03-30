0c7f63941b69fc333707cf6365ab80e658ca2d07
Jacob Craffey <craffeyj@mail.gvsu.edu>
2018-03-30 19:03:17 -0400

    Code Reformat Complete

--- classes/menu.py
+++ classes/menu.py
@@ -1,53 +1,9 @@
 from tkinter import *
 import tkinter.filedialog as filedialog
-import pyaudio
-import wave
 from classes.train import Train
 
 
 class Menu:
-    def test(self):
-        for i in range(10):
-            if i % 2 == 0:
-                record_audio(index=i)
-
-    def record_audio(self, index):
-        CHUNK = 1024
-        FORMAT = pyaudio.paInt16
-        CHANNELS = 2
-        RATE = 44100
-        RECORD_SECONDS = 2
-        WAVE_OUTPUT_FILENAME = "test_data/output" + str(index) + ".wav"
-
-        p = pyaudio.PyAudio()
-
-        stream = p.open(format=FORMAT,
-                        channels=CHANNELS,
-                        rate=RATE,
-                        input=True,
-                        frames_per_buffer=CHUNK)
-
-        print("* recording")
-
-        frames = []
-
-        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-            data = stream.read(CHUNK)
-            frames.append(data)
-
-        print("* done recording")
-
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
-
-        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
-        wf.close()
-
     def open_folder(self, label):
         try:
             f = filedialog.askopenfilename(filetypes=(("WAV files", "*.wav"), ("All files", "*.*")))
@@ -62,25 +18,18 @@ class Menu:
         self.root.title("BoomBap")
         self.root.minsize(width=400, height=400)
 
-        txt_kick = "No Kick Selected..."
-        txt_snare = "No Snare Selected..."
-        txt_hihat = "No HiHat Selected..."
-
-        lbl_kick = Label(self.root, text=txt_kick)
-        lbl_snare = Label(self.root, text=txt_snare)
-        lbl_hihat = Label(self.root, text=txt_hihat)
+        lbl_kick = Label(self.root, text="No Kick Selected..")
+        lbl_snare = Label(self.root, text="No Snare Selected...")
+        lbl_hihat = Label(self.root, text="No HiHat Selected...")
 
         btn_kick = Button(self.root, text="Add Kick", command=lambda: self.open_folder(lbl_kick))
         btn_snare = Button(self.root, text="Add Snare", command=lambda: self.open_folder(lbl_snare))
         btn_hihat = Button(self.root, text="Add HiHat", command=lambda: self.open_folder(lbl_hihat))
-        btn_tryout = Button(self.root, text="Try out the Instruments",
-                            command=lambda: try_instruments(lbl_kick["text"], lbl_snare["text"], lbl_hihat["text"]))
         btn_train = Button(self.root, text="Train Audio", command=lambda: Train())
 
         btn_kick.grid(row=0, column=0, padx=10, pady=10)
         btn_snare.grid(row=1, column=0, padx=10, pady=10)
         btn_hihat.grid(row=2, column=0, padx=10, pady=10)
-        btn_tryout.grid(row=4, column=0, padx=20, pady=20)
         btn_train.grid(row=5, column=0, padx=20, pady=20)
 
         lbl_kick.grid(row=0, column=1, padx=10, pady=10)
@@ -88,4 +37,3 @@ class Menu:
         lbl_hihat.grid(row=2, column=1, padx=10, pady=10)
 
         mainloop()
-
--- classes/train.py
+++ classes/train.py
@@ -1,3 +1,69 @@
+from tkinter import *
+import wave
+import pyaudio
+
+
 class Train:
+    def record_audio(self, index):
+        CHUNK = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 44100
+        RECORD_SECONDS = 2
+        WAVE_OUTPUT_FILENAME = "test_data/" + self.txt_dropdown.get() + "/" + str(index/2) + ".wav"
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        frames_per_buffer=CHUNK)
+
+        self.lbl_rec["text"] = "RECORDING"
+        print("* recording " + str(index/2))
+
+
+        frames = []
+
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
+
+        print("* done recording")
+        self.lbl_rec["text"] = "PAUSE"
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+
+    def test(self):
+        for i in range(10):
+            if i % 2 == 0:
+                self.record_audio(index=i)
+
     def __init__(self):
-        print("HELLO")
+        self.root = Tk()
+        self.root.title("Training")
+        self.root.minsize(width=300, height=300)
+
+        self.txt_dropdown = StringVar(self.root)
+        self.txt_dropdown.set("None")
+        self.dropdown = OptionMenu(self.root, self.txt_dropdown, "Kick", "Snare", "HiHat")
+
+        self.btn_record = Button(self.root, text="Record", command=lambda: self.test())
+
+        self.lbl_rec = Label(self.root, text="")
+
+        self.dropdown.pack()
+        self.btn_record.pack()
+        self.lbl_rec.pack()
+
+        mainloop()
